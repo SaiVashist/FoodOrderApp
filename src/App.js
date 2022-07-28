f88ba8ee45b8e14f7141cc5e80535e9e24e00a4c
Sai Vashist <saivashistdasyapu@gmail.com>
2022-07-28 18:24:50 +0530

    Pushing Home Page Changes
    Pushing Home Page Changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+import Header from './components/Layot/Header'
+function App () {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/components/Layot/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react"
+import mealsImage from '../../assets/dan-gold-4_jhDO54BYg-unsplash.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton"
+
+const Header = props => {
+
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Vashist's Restaurant</h1>
+                {/* <button>Cart</button> */}
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={ mealsImage} />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header
