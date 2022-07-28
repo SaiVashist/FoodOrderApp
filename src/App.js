5fa0bf04f32d8f47b52d7485bab8353817456a17
Sai Vashist <saivashistdasyapu@gmail.com>
2022-07-28 20:14:32 +0530

    Completed Meals Component
    Completed Meals Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Fragment } from 'react'
 import Header from './components/Layot/Header'
+import Meals from './components/Meals/Meals'
 function App () {
   return (
     <Fragment>
       <Header />
+      <main>
+<Meals/>
+      </main>
     </Fragment>
   )
 }
